643b96f0921c4702dc1e1dfb0f3b4b344a366b53
pankajkumar199 <pk228624@gmail.com>
2025-11-20 09:32:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,17 +11,16 @@ cubes.forEach(cube => {
     activeCube = e.target;
 
     const rect = activeCube.getBoundingClientRect();
+
     offsetX = e.clientX - rect.left;
     offsetY = e.clientY - rect.top;
 
-    activeCube.style.zIndex = 1000;
-    document.addEventListener("mousemove", drag);
-    document.addEventListener("mouseup", drop);
+    cube.style.zIndex = 999;
   });
 
 });
 
-function drag(e) {
+container.addEventListener("mousemove", (e) => {
   if (!activeCube) return;
 
   const containerRect = container.getBoundingClientRect();
@@ -29,18 +28,14 @@ function drag(e) {
   let x = e.clientX - containerRect.left - offsetX;
   let y = e.clientY - containerRect.top - offsetY;
 
-  // boundaries
   x = Math.max(0, Math.min(x, containerRect.width - activeCube.offsetWidth));
   y = Math.max(0, Math.min(y, containerRect.height - activeCube.offsetHeight));
 
   activeCube.style.left = x + "px";
   activeCube.style.top = y + "px";
-}
+});
 
-function drop() {
+container.addEventListener("mouseup", () => {
   if (activeCube) activeCube.style.zIndex = 1;
-
   activeCube = null;
-  document.removeEventListener("mousemove", drag);
-  document.removeEventListener("mouseup", drop);
-}
+});
