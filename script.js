bf809435a07646be2cad3624a8351d2a1f3291c5
pankajkumar199 <pk228624@gmail.com>
2025-11-20 10:02:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 const list = document.querySelector('.items');
-let dragging = null;
+let dragSrc = null;
 
 document.querySelectorAll('.item').forEach(item => {
 
   item.addEventListener('dragstart', e => {
-    dragging = item;
+    dragSrc = item;
     item.classList.add('dragging');
     e.dataTransfer.setData('text/plain', '');
-  });
-
-  item.addEventListener('dragenter', e => {
-    if (item !== dragging) item.classList.add('over');
-  });
-
-  item.addEventListener('dragleave', e => {
-    item.classList.remove('over');
   });
 
   item.addEventListener('dragover', e => {
     e.preventDefault();
   });
 
+  item.addEventListener('dragenter', e => {
+    if (item !== dragSrc) {
+      item.classList.add('over');
+    }
+  });
+
+  item.addEventListener('dragleave', e => {
+    item.classList.remove('over');
+  });
+
   item.addEventListener('drop', e => {
     e.preventDefault();
-    if (item !== dragging) {
-      list.insertBefore(dragging, item);
+    if (item !== dragSrc) {
+      list.insertBefore(dragSrc, item);
     }
     item.classList.remove('over');
   });
 
   item.addEventListener('dragend', e => {
     item.classList.remove('dragging');
+    document.querySelectorAll('.item').forEach(i => i.classList.remove('over'));
   });
 
 });
